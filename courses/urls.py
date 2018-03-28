ef1ea8df2dba052ed40bfac6d7b8a942f33d09e0
josh thorn <joshthorn91@gmail.com>
2018-03-28 14:29:34 +0100

    Gernic CRUD
    changed views to use rest_framework generics

--- courses/urls.py
+++ courses/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	  url(r'^$', views.ListCreateCourse.as_view(), name='course_list')
+	  url(r'^$', views.ListCreateCourse.as_view(), name='course_list'),
+	  url(r'^(?P<pk>\d+)/$',
+	  views.RetrieveUpdateDestroyCourse.as_view(),
+	   name='course_detail'),
 ]
