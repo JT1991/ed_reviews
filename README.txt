114909742871bec0669cfdf69eefd9a9ec9398cd
josh thorn <joshthorn91@gmail.com>
2018-03-29 10:22:02 +0100

    6
    Pagination

--- README.txt
+++ README.txt
@@ -1,5 +1,3 @@
-Default superuser
+superuser
 -----------------
-username: kennethlove
-password: testpassword
-token:
+username:josh password: mhlover12 token:a74e42664e4d0d26f9d1e9cca877e5d31b0eb1d0	
--- ed_reviews/settings.py
+++ ed_reviews/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-		'rest_framework',
+    'rest_framework',
+    'rest_framework.authtoken',
     'courses',
 ]
 
@@ -125,10 +126,12 @@ STATIC_URL = '/static/'
 # REST FRAMEWORK
 REST_FRAMEWORK = {
 	'DEFAULT_AUTHENTICATION_CLASSES': (
-		'rest_framework.authentication.SessionAuthentication',
+		'rest_framework.authentication.TokenAuthentication',
 	),
 	'DEFAULT_PERMISSION_CLASSES': (
 		'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-	)
+	),
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 2
 }
 	
